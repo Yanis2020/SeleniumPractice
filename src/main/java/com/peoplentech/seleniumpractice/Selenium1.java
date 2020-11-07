ce271ec3defadf0c19defb4fd519576baee1a3cd
Yanis2020 <amimeuryanis@gmail.com>
2020-11-07 13:19:45 -0600

    initial selenium

---
+++ src/main/java/com/peoplentech/seleniumpractice/Selenium1.java
@@ -0,0 +1,26 @@
+package com.peoplentech.seleniumpractice;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class Selenium1 {
+    public static void main(String[] args) throws InterruptedException {
+
+        // set up date as chrome driver needs
+        System.setProperty("webdriver.chrome.driver", "src/main/resources/chromedriver.exe");
+
+        // create object for chrome driver
+        WebDriver driver = new ChromeDriver();
+        driver.get("https://www.google.com");
+
+        // wait 5 seconds
+        Thread.sleep(5000);
+
+        // close chromr
+        driver.close();
+
+
+    }
+
+
+}
